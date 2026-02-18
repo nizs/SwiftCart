256e237f4a89d781840894b4e9a98d4c2280e1d6
Tauhid Afser <tauhidafser03@gmail.com>
2026-02-18 15:16:47 +0600

    all button and functionality, footer, modal close btn error fixed

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // loading trending data
 const loadTrending = () => {
-    const trending = "https://fakestoreapi.com/products";
-    fetch(trending)
-        .then(res => res.json())
-        .then(data => displayTrending(data))
+  const trending = "https://fakestoreapi.com/products";
+  fetch(trending)
+    .then(res => res.json())
+    .then(data => displayTrending(data))
 }
 // displaying teending data
 const displayTrending = trendings => {
-    const trendyData = trendings.slice(0, 3);
-    const trendingContainer = document.getElementById('trending-container');
-    if (!trendingContainer) return;
-    for (let trend of trendyData) {
-        // console.log(trend);
-        const trendingCard = document.createElement("div");
-        trendingCard.innerHTML = `
+  const trendyData = trendings.slice(0, 3);
+  const trendingContainer = document.getElementById('trending-container');
+  if (!trendingContainer) return;
+  for (let trend of trendyData) {
+    // console.log(trend);
+    const trendingCard = document.createElement("div");
+    trendingCard.innerHTML = `
         <div class="card bg-white shadow-sm">
   <figure>
     <img
@@ -43,74 +43,141 @@ const displayTrending = trendings => {
   </div>
 </div>
         `;
-        trendingContainer.append(trendingCard);
-    }
+    trendingContainer.append(trendingCard);
+  }
 }
 loadTrending();
 
+//Load All products data
+const loadAllProducts = () => {
+  const allProducts = "https://fakestoreapi.com/products";
+
+  fetch(allProducts)
+    .then(res => res.json())
+    .then(data => displayAllProducts(data))
+}
+//Display All products
+const displayAllProducts = (products) => {
+  //1. get the container and empty
+  const productsContainer = document.getElementById('products-container');
+  productsContainer.innerHTML = "";
+  if (products.length == 0) {
+    productsContainer.innerHTML = `
+         <div class="text-center bg-sky-100 col-span-full rounded-xl py-10 space-y-6 font-bangla">
+         <p class="text-xl font-medium text-black">No Category has been selected.Please seclect a category above</p>
+         <h2 class="font-bold text-4xl">নেক্সট <span class="text-primary">Lesson</span> এ যান</h2>
+     </div>
+         `
+    return;
+  }
+  // 2.get level words from having a loop
+  for (let product of products) {
+    // console.log(product);
+    //3.create element
+    const productCard = document.createElement('div');
+    productCard.innerHTML = `
+         <div class="card bg-white shadow-sm">
+         <figure>
+           <img
+               src="${product.image}"
+               class='p-12 bg-gray-300 '
+           />
+         </figure>
+         <div class="card-body">
+           <div class="flex justify-between">
+             <div class="badge bg-[#E0E7FF]">${product.category}</div>
+             <div class="">
+             <span><i class="text-yellow-500 fa-solid fa-star"></i></span>
+             <span>${product.rating.rate}</span>
+             <span>(${product.rating.count})</span>
+             </div>
+           </div>
+           <p>${product.title.slice(0, 30)}...</p>
+           <h2 class="card-title">$${product.price}</h2>
+           <div class="card-actions justify-between">
+             <button onClick="loadProductDetails(${product.id})" class="btn btn-outline text-[12px]">
+             <i class="fa-regular fa-eye"></i>
+             Details</button>
+             <button class="btn btn-primary text-[12px]">
+             <i class="fa-solid fa-cart-shopping"></i>
+             Add to Cart</button>
+           </div>
+         </div>
+       </div>
+         `
+    //4.Appned into container
+    productsContainer.append(productCard);
+  }
+}
 
 // Load products btn by category
 const loadProductsBtn = () => {
-    const allproductsBtn = 'https://fakestoreapi.com/products/categories';
+  const allproductsBtn = 'https://fakestoreapi.com/products/categories';
 
-    fetch(allproductsBtn)
-        .then(res => res.json())
-        .then(data => displayProductsBtns(data))
+  fetch(allproductsBtn)
+    .then(res => res.json())
+    .then(data => displayProductsBtns(data))
 }
 // display Product buttons by category
 const displayProductsBtns = btns => {
-    //1. get the container and empty
-    const productBtnContainer = document.getElementById('productBtn-container');
-    productBtnContainer.innerHTML = "";
-    // 2.get lesson from having a loop
-    for (let btn of btns) {
-        // console.log(btn);
-        //3.create element
-        const btnDiv = document.createElement('div');
-        btnDiv.innerHTML = `
+  //1. get the container and empty
+  const productBtnContainer = document.getElementById('productBtn-container');
+  productBtnContainer.innerHTML = "";
+  btns.unshift("All");
+  // 2.get lesson from having a loop
+  for (let btn of btns) {
+    // console.log(btn);
+    //3.create element
+    const btnDiv = document.createElement('div');
+    btnDiv.innerHTML = `
         <button class="btn btn-outline btn-primary hover:text-white rounded-xl">${btn}</button>
         `;
-        const categoryButton = btnDiv.querySelector('button');
-        categoryButton.addEventListener('click', () => {
-            loadProducts(btn);
-        });
+    const categoryButton = btnDiv.querySelector('button');
+    categoryButton.addEventListener('click', () => {
+      if (btn === "All") {
+        loadAllProducts();   // 🔥 call special function
+      } else {
+        loadProducts(btn);   // existing category loader
+      }
 
-        //4.Appned into container
-        productBtnContainer.append(btnDiv);
-    }
+    });
+
+    //4.Appned into container
+    productBtnContainer.append(btnDiv);
+  }
 }
 
 loadProductsBtn();
 
 // load products data by category
 const loadProducts = category => {
-    const products = `https://fakestoreapi.com/products/category/${category}`;
+  const products = `https://fakestoreapi.com/products/category/${category}`;
 
-    fetch(products)
-        .then(res => res.json())
-        .then(data => displayProducts(data))
+  fetch(products)
+    .then(res => res.json())
+    .then(data => displayProducts(data))
 }
 
 // Display levele words
 const displayProducts = products => {
-    //1. get the container and empty
-    const productsContainer = document.getElementById('products-container');
-    productsContainer.innerHTML = "";
-    if (products.length == 0) {
-        productsContainer.innerHTML = `
+  //1. get the container and empty
+  const productsContainer = document.getElementById('products-container');
+  productsContainer.innerHTML = "";
+  if (products.length == 0) {
+    productsContainer.innerHTML = `
         <div class="text-center bg-sky-100 col-span-full rounded-xl py-10 space-y-6 font-bangla">
         <p class="text-xl font-medium text-black">No Category has been selected.Please seclect a category above</p>
         <h2 class="font-bold text-4xl">নেক্সট <span class="text-primary">Lesson</span> এ যান</h2>
     </div>
         `
-        return;
-    }
-    // 2.get level words from having a loop
-    for (let product of products) {
-        // console.log(product);
-        //3.create element
-        const productCard = document.createElement('div');
-        productCard.innerHTML = `
+    return;
+  }
+  // 2.get level words from having a loop
+  for (let product of products) {
+    // console.log(product);
+    //3.create element
+    const productCard = document.createElement('div');
+    productCard.innerHTML = `
         <div class="card bg-white shadow-sm">
         <figure>
           <img
@@ -140,23 +207,23 @@ const displayProducts = products => {
         </div>
       </div>
         `
-        //4.Appned into container
-        productsContainer.append(productCard);
-    }
+    //4.Appned into container
+    productsContainer.append(productCard);
+  }
 
 }
 
 const loadProductDetails = id => {
-    const productDetail = `https://fakestoreapi.com/products/${id}`;
+  const productDetail = `https://fakestoreapi.com/products/${id}`;
 
-    fetch(productDetail)
-        .then(res => res.json())
-        .then(data => displayProductDetails(data))
+  fetch(productDetail)
+    .then(res => res.json())
+    .then(data => displayProductDetails(data))
 }
 
 const displayProductDetails = product => {
-    const productDetails = document.getElementById("product-details-container");
-    productDetails.innerHTML = `<div class="card bg-base-100 card-lg shadow-sm">
+  const productDetails = document.getElementById("product-details-container");
+  productDetails.innerHTML = `<div class="card bg-base-100 card-lg shadow-sm">
     <div class="card-body">
       <h2 class="card-title">${product.title}</h2>
       <p>Description: ${product.description.slice(0, 20)}</p>
@@ -166,5 +233,6 @@ const displayProductDetails = product => {
         <button class="btn btn-primary">Buy Now</button>
       </div>
     </div>`;
-    document.getElementById("product_detals_modal").showModal();
+
+  document.getElementById("product_details_modal").showModal();
 }
